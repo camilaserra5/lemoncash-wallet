6879c01d091dee1eeebf3405e89b612837aa019d
Cami Serra <camilaserra5@gmail.com>
2022-02-05 23:42:15 -0300

    add clases and packages for tests

---
+++ src/test/java/com/lemoncash/wallet/user/UserMapperTest.java
@@ -0,0 +1,8 @@
+package com.lemoncash.wallet.user;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class UserMapperTest {
+}
---
+++ src/test/java/com/lemoncash/wallet/util/PasswordEncrypterTest.java
@@ -0,0 +1,8 @@
+package com.lemoncash.wallet.util;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class PasswordEncrypterTest {
+}
